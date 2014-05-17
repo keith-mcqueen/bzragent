ba8d673dffaa8a399b1ab9ca63c38451755be8e3
eric <eric@eric-VirtualBox.(none)>
2014-05-17 16:35:46 -0600

    making changes so that worldmap works with obstaclesfieldgen

--- bzagents/obstaclesfieldgen.py
+++ bzagents/obstaclesfieldgen.py
@@ -2,7 +2,7 @@
 
 from vec2d import Vec2d
 from masterfieldgen import FieldGen
-
+from worldmap import WorldMap
 
 class ObstaclesFieldGen(FieldGen):
     def __init__(self, bzrc, default_factor=1):
@@ -13,36 +13,16 @@ class ObstaclesFieldGen(FieldGen):
         self.default_factor = default_factor
         self.obstacles = self.bzrc.get_obstacles()
         self.shoot = False
+        self.world_map = WorldMap(self.bzrc)
 
     def vector_at(self, x, y):
         factor = self.default_factor
-
-        # for each obstacle, compute it's effect at the given location
-        for obstacle in self.obstacles:
-            north = obstacle[0][1]
-            east = obstacle[0][0]
-            south = obstacle[2][1]
-            west = obstacle[2][0]
-
-            # if the point is between the north and the south, then ...
-            if south <= y <= north:
-                # if the point is within the margin on the west, then return a westward vector
-                if west - self.offset < x < west:
-                    return Vec2d(-self.force, 0) * factor, self.shoot
-                # or if the point is within the margin on the east, then return an eastward vector
-                elif east < x < east + self.offset:
-                    return Vec2d(self.force, 0) * factor, self.shoot
-            # or if the point is between the east and the west, then ...
-            elif west <= x <= east:
-                # if the point is within the margin on the south, then return a southward vector
-                if south - self.offset < y < south:
-                    return Vec2d(0, -self.force) * factor, self.shoot
-                # or if the point is within the margin on the north, then return a northward vector
-                elif north < y < north + self.offset:
-                    return Vec2d(0, self.force) * factor, self.shoot
-
-        return Vec2d(0, 0), self.shoot
-
+        closest_edge = self.world_map.obstacle_edge_at(x, y, self.offset)
+        if closest_edge == None:
+			return Vec2d(0, 0), self.shoot 
+		
+        return closest_edge.perpendicular_normal(), self.shoot
+        
 
 class ObstaclesFieldGen2(FieldGen):
     def __init__(self, bzrc, default_factor=1):
--- bzagents/worldmap.py
+++ bzagents/worldmap.py
@@ -6,12 +6,13 @@ from masterfieldgen import FieldGen
 
 class WorldMap(object):
     def __init__(self, bzrc):
-		self.bzrc = bzrc
-		self.edges = []
-		for obstacle in bzrc.get_obstacles():
-			length = len(obstacle)
-			for i in range(0,length):
-				self.edges.append([(obstacle[i%length],obstacle[(i+1)%length])])        
+        self.bzrc = bzrc
+        self.edges = []
+        for obstacle in bzrc.get_obstacles():
+            length = len(obstacle)
+            for i in range(0,length):
+                self.edges.append((obstacle[i%length],obstacle[(i+1)%length]))
+                    
 
     def obstacle_edge_at(self, x, y, distance):
         best_distance = distance
@@ -20,41 +21,39 @@ class WorldMap(object):
         # for each obstacle, compute it's effect at the given location
         for edge in self.edges:
 			# get the distance from (x,y) to the edge
-			current_distance, is_inside_edge = get_distance(x,y,edge)		
-		    
-		    if not is_inside_edge:
-				continue
+            current_distance, is_inside_edge = self.retrieve_distance(x,y,edge)		
+            if not is_inside_edge:
+                continue
 				
 			# if distance > given distance then skip
-			if current_distance > distance:
-				continue
+            if current_distance > distance:
+                continue
 				
 			# if distance > best current distance then skip
-			if current_distance > best_distance:
-				continue
+            if current_distance > best_distance:
+                continue
 				 
 			# update best current distance and save nearest edge
-			best_distance = current_distance
-			nearest_edge = edge
+            best_distance = current_distance
+            nearest_edge = edge
 			
 		# return nearest edge
         return nearest_edge
 	
-	def get_distance(self, x, y, edge):
+    def retrieve_distance(self, x, y, edge):
 		# calculate the vector between the edge endpoints
-		edge_vector = Vec2d(edge[0][0]-edge[1][0],edge[0][1] - edge[1][1])
+        edge_vector = Vec2d(edge[0][0]-edge[1][0],edge[0][1] - edge[1][1])
 		
 		# calculate the normal vector to the edge
-		n = edge_vector.perpendicular_normal()
+        n = edge_vector.perpendicular_normal()
 		
 		# calculate the edge's distance from origin
-		p1 = Vec2d(edge[0][0],edge[0][1])
-		d = p1.dot(n)
+        p1 = Vec2d(edge[0][0],edge[0][1])
+        d = p1.dot(n)
 		
 		# calculate the distance from the point to the edge
-		q = Vec2d(x,y)
-		q_dot_n = q.dot(n)
-		q_prime = q + (d - q_dot_n)*n
-		
-		return q_dot_n - d, min(edge[0][0], edge[1][0]) < q_prime[0] < max(edge[0][0], edge[1][0]) 
-			            and min(edge[0][1], edge[1][1]) < q_prime[1] < max(edge[0][1], edge[1][1])
+        q = Vec2d(x,y)
+        q_dot_n = q.dot(n)
+        q_prime = q + (d - q_dot_n)*n
+        is_inside = min(edge[0][0], edge[1][0]) < q_prime[0] < max(edge[0][0], edge[1][0]) and min(edge[0][1], edge[1][1]) < q_prime[1] < max(edge[0][1], edge[1][1])
+        return q_dot_n - d, is_inside
