0e8d252b75fa1ab96aab6e23aedbcd53717a325d
keith-mcqueen <kemcqueen@mac.com>
2014-06-03 23:41:41 -0600

    Cleaned up the TrackEnemyFieldGen to remove unnecessary code and complexity.

--- bzagents/trackenemyfieldgen.py
+++ bzagents/trackenemyfieldgen.py
@@ -5,7 +5,6 @@ import numpy
 from numpy import matrix
 from numpy import linalg
 
-from vec2d import Vec2d
 from masterfieldgen import FieldGen
 
 
@@ -24,7 +23,7 @@ class TrackEnemyFieldGen(FieldGen):
         for base in bases:
             if base.color == tank.color:
                 enemy_base = base
-        #Matrices for Kalman Agent
+        # Matrices for Kalman Agent
         self.mu_t = matrix([[enemy_base.x],
                             [0],
                             [0],
@@ -72,39 +71,15 @@ class TrackEnemyFieldGen(FieldGen):
             k_matrix = (F_sigma).dot(self.H_T)
             inverse = linalg.inv(self.H.dot(F_sigma).dot(self.H_T) + self.sigma_z)
             k_matrix = k_matrix.dot(inverse)
-            self.mu_t = self.F.dot(self.mu_t) + k_matrix.dot(
-                matrix([[tank.x], [tank.y]]) - self.H.dot(self.F).dot(self.mu_t))
+            # self.mu_t = self.F.dot(self.mu_t) +
+            # k_matrix.dot(matrix([[tank.x], [tank.y]]) -
+            # self.H.dot(self.F).dot(self.mu_t))
+            self.mu_t = self.F.dot(self.mu_t)
             self.sigma_t = (numpy.identity(6) - k_matrix.dot(self.H)).dot(F_sigma)
 
-        factor = self.default_factor
-
-        # create a vector for the given location
-        location_vector = Vec2d(x, y)
-
-        # prepare the resultant vector
-        resultant_vector = Vec2d(0, 0)
-
-        # determine if we should shoot
-        shoot = False
-
-        # create a vector for the tank's position
-        tank_vector = Vec2d(self.mu_t.item(0), self.mu_t.item(3))
-
         # get the distance between the current tank and the given location
-        distance = location_vector.get_distance(tank_vector)
+        # distance = location_vector.get_distance(tank_vector)
         # normalize the vector to the tank
-        force_vector = (tank_vector - location_vector).normalized()
+        force_vector = (self.mu_t.item(0) - x, self.mu_t.item(3) - y)
 
-        # scale the vector according as a function of distance
-        if distance < self.threshold:
-            scale = max(distance / self.threshold, self.min_scale)
-        else:
-            scale = 1
-
-        if distance < self.shooting_range:
-            shoot = shoot or True
-
-        # add the vector to the resultant
-        resultant_vector += force_vector * scale * factor
-
-        return resultant_vector, shoot
+        return force_vector, True
